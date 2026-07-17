e3554b60f27e83d49f8161b2254001d16be3c3ee
Shahid09092005 <shahidmansuri4444@gmail.com>
2026-07-17 06:17:31 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Create an arrow function
+const App = () => {
+    return (
+        <>
+        <div> Hello asdf</div>
+        <div>
+           sdfasdf
+           <h1>Pushing to Github</h1>
+        </div>
+        </>
+
+    )
+}
+export default App
